515bec5ca24127323bb3c12a5f16b967b880c271
Eunsol Lee <73922496+essol2@users.noreply.github.com>
2021-03-31 16:54:40 +0900

    [test of gitignore]

--- src/main/java/com/kh/samdado/route/controller/RouteController.java
+++ src/main/java/com/kh/samdado/route/controller/RouteController.java
@@ -1,4 +1,4 @@
-package com.kh.samdado.route;
+package com.kh.samdado.route.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
