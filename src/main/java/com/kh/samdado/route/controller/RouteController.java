085b9ffc555ca33098e756bee8fa5be6635469a1
han-bbak <letdior@naver.com>
2021-03-31 18:11:28 +0900

    [충돌 때문에 하는 푸시]
    푸시

--- src/main/java/com/kh/samdado/route/controller/RouteController.java
+++ src/main/java/com/kh/samdado/route/controller/RouteController.java
@@ -1,4 +1,4 @@
-package com.kh.samdado.route;
+package com.kh.samdado.route.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +7,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/route")
 public class RouteController {
-	@GetMapping("/m_route")
+	
+	@GetMapping("/m_route")				// 길 만들기 페이지로 이동
 	public String route() {
 		return "route/route_main";
-		
 	}
+	
+	public String searchRoute() {		// 루트 검색
+		return "";
+	}
+	
+	public String deleteSpot() {		// 여행지 삭제
+		return "";
+	}
+	
+	public String searchSpot() {		// 여행지 검색
+		return "";
+	}
+	
+	public String addSpot() {			// 여행지 추가
+		return "";
+	}
+	
+	public String changeRoutePage() {	// 여행지 순서 바꾸기 페이지로 이동
+		return "";
+	}
+	
+	public String changeRoute() {		// 여행지 순서 바꾸기
+		return "";
+	}
+	
+	public String addRoute() {			// 루트 저장하기
+		return "";
+	}
+	
+	public String goRoom() {			// 숙박 페이지로 이동
+		return "";
+	}
+	
+	
 }
