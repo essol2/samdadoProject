0963be150e06eed353b8d51128fb6386177d71b5
Eunsol Lee <73922496+essol2@users.noreply.github.com>
2021-04-01 23:18:21 +0900

    [결제API 연동]
    Booking, Income, Alert, Point vo 생성
    결제API 연동 성공!
    아직 DB연결은 안함

--- src/main/java/com/kh/samdado/mypage/model/dao/MypageDaoImpl.java
+++ src/main/java/com/kh/samdado/mypage/model/dao/MypageDaoImpl.java
@@ -1,9 +1,19 @@
 package com.kh.samdado.mypage.model.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kh.samdado.bussiness.model.vo.Alliance;
+import com.kh.samdado.bussiness.model.vo.Jjim;
+import com.kh.samdado.bussiness.model.vo.bussiness.Bussiness;
+import com.kh.samdado.mypage.model.vo.AccountBook;
+import com.kh.samdado.mypage.model.vo.Alert;
+import com.kh.samdado.mypage.model.vo.Booking;
+import com.kh.samdado.mypage.model.vo.Point;
 import com.kh.samdado.mypage.model.vo.QnA;
 import com.kh.samdado.user.model.vo.User;
 
@@ -17,12 +27,58 @@ public class MypageDaoImpl implements MypageDao{
 	public int updateUserInfo(User u) {
 		return sqlSession.update("mypageMapper.updateUserInfo", u);
 	}
-	// 2. 문의 DB에 저장하는 메소드
+	
+	// 알림 select 메소드
+	@Override
+	public List<Alert> selectAlertLis(String usno) {
+		return sqlSession.selectList("mypageMapper.selectAlertLis", usno);
+	}
+	
+	// 제휴회원 - 광고 관리 메소드
+	@Override
+	public List<Alliance> selectAdvertList(String usno) {
+		return sqlSession.selectList("mypageMapper.selectAdvertList", usno);
+	}
+	
+	// 제휴회원 - 포인트 관리 메소드
+	@Override
+	public List<Point> selectPointList(String usno) {
+		return sqlSession.selectList("mypageMapper.selectPointList", usno);
+	}
+	
+	// 제휴회원 - 사업장 조회 메소드
+	@Override
+	public List<Bussiness> selectBussList(String usno) {
+		return sqlSession.selectList("mypageMapper.selectBussList", usno);
+	}
+	
+	// 제휴회원 - 문의 DB에 저장하는 메소드
 	@Override
 	public int insertNewQnA(QnA qna) {
 		return sqlSession.insert("mypageMapper.insertNewQnA", qna);
 	}
+	
+	// 일반회원 - 찜목록 출력 메소드
+	@Override
+	public List<Jjim> selctJjimList(String usno) {
+		return sqlSession.selectList("mypageMapper.selctJjimList", usno);
+	}
+	
+	// 일반회원 - 내 예약 목록 출력 메소드
+	@Override
+	public List<Booking> selectBookList(String usno) {
+		return sqlSession.selectList("mypageMapper.selectBookList", usno);
+	}
+	
+	// 일반회원 - 가계부 조회 메소드
+	@Override
+	public List<AccountBook> selectAccountList(String usno) {
+		return sqlSession.selectList("mypageMapper.selectAccountList", usno);
+	}
 
-	
-	
+	// 일반회원 - 가계부 입력 메소드
+	@Override
+	public int insertNewAcc(AccountBook acc) {
+		return sqlSession.insert("mypageMapper.selectAccountList", acc);
+	}	
 }
